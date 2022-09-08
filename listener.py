f92838e0607f8a8cbe897813634db1c860f5ec93
Kevin Burgon <kevin.burgon@gmail.com>
2022-09-07 21:59:41 -0600

    Create listener callback for MQTT
    Can actually be greatly simplified by using the blocking simple call.
    Refer to documentation on how to set up the `simple` method

--- listener.py
+++ listener.py
@@ -1,15 +1,30 @@
+from ast import If
 import paho.mqtt.client as mqtt
 import time
 
-def publish_message(client, message):
-    client.publish("test_topic", message)
-    print("Published " + message + " to topic 'TEST_TOPIC'")
+def publish_message(client, topic, message):
+    client.publish(topic, message)
+    print("Published " + message + " to topic " + topic + ".")
     time.sleep(1)
+
+def on_message_received(client, userdata, message):
+    print('Received message ' + str(message.payload) + ' from topic ' + str(message.topic) + '.')
+    if message.payload == 'detected':
+        print('Motion detected, playing sound...')
+    else:
+        print('No motion detected')
+
+def setup_client(brokerAddress):
+    client = mqtt.Client()
+    client.connect(brokerAddress)
+    client.subscribe('porch/dorbell/movement/status')
+    client.on_message = on_message_received
+    return client
 
 if (__name__ == "__main__"):
     mqttBroker = '192.168.1.100'
-    client = mqtt.Client()
-    client.connect(mqttBroker)
-
-    message = input('Enter message: ')
-    publish_message(client, message)
+    heartbeatTopic = 'porch/frankensteinsdoorbell/heartbeat'
+    client = setup_client(mqttBroker)
+    while True:
+        publish_message(client, heartbeatTopic, str(time.localtime))
+        time.sleep(10)
