0b06da4fa74c30bfb53ca62eec3c68b9a12ad7c9
Kevin Burgon <kevin.burgon@gmail.com>
2022-09-14 21:08:41 -0600

    Add logic which plays sounds
    Currently may be broken as the submodule is struggling to find the pip
    library

--- listener.py
+++ listener.py
@@ -1,13 +1,19 @@
+import sys
+sys.path.insert(0, 'sound-playback-helper')
+
 from ast import If
 from datetime import datetime
+from sound_player import SoundPlayer
 import paho.mqtt.client as mqtt
 import time
+
 
 def on_message_received(client, userdata, message):
     message_text = message.payload.decode('utf-8')
     print('Received message ' + message_text + ' from topic ' + str(message.topic) + '.')
     if message_text == 'detected':
         print('Motion detected, playing sound...')
+
     else:
         print('No motion detected')
 
